6492d1acaac1e4395ce40192cb37bd539d22fec2
Jusxin <72774910+Jusxin@users.noreply.github.com>
2021-09-04 17:35:14 -0400

    finished, needs some optimization and shorter line

--- Jvstinoo's Project #1/main.py
+++ Jvstinoo's Project #1/main.py
@@ -12,27 +12,71 @@ For now, I'll start by doing these.
 options = {'1) Deposit funds into account': '1', '2) Withdraw funds from account': '2', '3) Quit.':'3'}
 
 Idea: Do three loops, for each of the options and just run through them depending on the choice of user...
-'''
 
-name = input('Please enter the name for your account: ')
-balance = float(input('Please enter your starting balance: '))
+Strip input or lowercase, uppercase; all things like that. If not number, repeat shit like that
 
-options = ['Account name: ' + name, 'Account balance: ' + str(balance), 'You may: ', '1) Deposit funds into account',
-           '2) Withdraw funds from account', '3) Quit.']
-
-if(name and balance):
-    for nums in options:
-        print(nums)
-    selection = input('Please enter your selection: ')
-    if(selection == '1'):
-        deposit = float(input('Amount you wish to deposit: '))
-        balance += deposit
-        print('$', balance)
-    print(nums)
-    if(selection == '2'):
+if(selection == '2'):
         withdraw = float(input('Amount you wish to withdraw: '))
         if(withdraw > balance):
             print('Insufficient funds. For your convenience, an overdraft fee of $5 is being deducted from your balance. Have a nice day!!')
             balance -= 5
             print(nums)
         selection = input('Please enter your selection: ')
+'''
+name = input('Please enter the name for your account: ')
+balance = float(input('Please enter your starting balance: '))
+
+n_deposits = 0
+n_withdrawals = 0
+bad_withdrawals = 0
+fees = 0
+
+d_total = 0
+w_total = 0
+bw_total = 0
+f_total = 0
+
+
+def menu():
+    global balance
+    options = ['Account name: ' + name, 'Account balance: $' + str(balance), 'You may: ', '1) Deposit funds into account',
+               '2) Withdraw funds from account', '3) Quit.']
+    for choices in options:
+        print(choices)
+
+
+def final_statement():
+    global balance
+    quit = ['Final Statement', 'Account name: ' + name, 'Account balance: $' + str(balance), str(n_deposits) + ' deposits totaling $' + str(d_total), str(
+        n_withdrawals)+' withdrawals totaling $' + str(w_total), str(bad_withdrawals) + ' bad withdrawals totaling $' + str(bw_total), str(fees) + ' fees totaling $' + str(f_total)]
+    for options in quit:
+        print(options)
+
+
+while(name and balance):
+    menu()
+    selection = input('Please enter your selection: ')
+    if(selection == '1'):
+        deposit = float(input('Amount you wish to deposit: '))
+        balance += deposit
+        d_total += deposit
+        n_deposits += 1
+
+    elif(selection == '2'):
+        withdraw = float(input('Amount you wish to withdraw: '))
+        if(withdraw > balance):
+            print('Insufficient funds. An overdraft fee of $5 is being deducted from your balance. \nHave a nice day!!')
+            balance -= 5
+            fees += 1
+            bad_withdrawals += 1
+            bw_total += withdraw
+            f_total += 5
+
+        elif(withdraw <= balance):
+            balance -= withdraw
+            n_withdrawals += 1
+            w_total += withdraw
+
+    elif(selection == '3'):
+        final_statement()
+        break
